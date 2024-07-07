259731775ec138869bed61e9099ea5f96dff98ec
luizdebem <luizdebem99@gmail.com>
2024-07-07 18:36:38 -0300

    Capítulo 10 (APIs): de handlers até web frameworks vs frameworks.

--- 10-apis/cmd/server/main.go
+++ 10-apis/cmd/server/main.go
@@ -2,9 +2,40 @@
 
 package main
 
-import "10-apis/configs"
+import (
+	"10-apis/configs"
+	"10-apis/internal/entity"
+	"10-apis/internal/infra/db"
+	"10-apis/internal/infra/webserver/handlers"
+	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBHost)
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	println(config)
+
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	database.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := db.NewProductDB(database)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
+
+// Ver Go Fiber, Go Echo, Gin
+// frameworks web server
+
+// Ver Buffalo
+// framework Go completo
---
+++ 10-apis/internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"10-apis/internal/dto"
+	"10-apis/internal/entity"
+	"10-apis/internal/infra/db"
+	"encoding/json"
+	"net/http"
+)
+
+type ProductHandler struct {
+	ProductDB db.ProductInterface
+}
+
+func NewProductHandler(db db.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
