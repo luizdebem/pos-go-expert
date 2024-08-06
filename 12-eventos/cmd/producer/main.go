f81667efe126633ab96b2abc1d5de824315bfc35
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2024-08-06 19:33:01 -0300

    Capítulo 12 (eventos) finalizado. Aplicação de RabbitMQ, compreensão de channel, producer, consumer, exchange, queue.

---
+++ 12-eventos/cmd/producer/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "luizdebem.com/pos-go/utils/pkg/rabbitmq"
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+}
